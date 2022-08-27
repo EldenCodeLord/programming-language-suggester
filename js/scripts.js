f6a19a4f476ea2925b9cd67eea2bb543276f2374
Manuel Raygoza <manny.raygoza@gmail.com>
2022-08-27 13:39:03 -0700

    WIP: Added eventlisteners for load fucntion and submit function.

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+//Business Logic:
+
+//User Interface Logic:
+
+window.addEventListener("load", function() {
+  let form = document.querySelector("form");
+  let resetBtn = document.querySelector("button#reset");
+  let result = document.querySelector("div#result");
+
+  form.addEventListener("submit", function() {
+    const personInput = document.getElementById("personInput").value;
+    const animal = document.getElementById("animal").value;
+    const food = document.getElementById("food").value;
+
+
+    document.querySelector("personInput").innerText = personInput;
+    document.querySelector("animal").innerText = animal;
+    document.querySelector("food").innerText = food;
+    
+    e.preventDefault();
+  }
+}
