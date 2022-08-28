b596bb108e5c69c106766dbf43cfb00ff62eeccd
Manuel Raygoza <manny.raygoza@gmail.com>
2022-08-27 23:09:31 -0700

    WIP: Trying to fix broken form eventlistener.

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ function hideResult() {
 }
 
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function(e) {
   let form = document.querySelector("form");
   let resetBtn = document.querySelector("button#reset");
   let result = document.querySelector("div#result");
@@ -19,25 +19,37 @@ window.addEventListener("load", function() {
     const nameInput = document.getElementById("nameInput").value;
     const animal = document.getElementById("animal").value;
     const food = document.getElementById("food").value;
+    const pizza = document.getElementById("js", "food").value;
+    const cat = document.getElementById("css", "animal").value;
+    const dog = document.querySelector("html", "animal").value;
+      if (dog === "html" && Dog === "html") {
+        html.removeAttribute("class", "hidden");
+      } else if (cat === "css" && Cat === "css"){
+        css.removeAttribute("class", "hidden");
+      } else if ("yes" === true && "Yes" === true|| "no" === false && "No" === false) {
+        js.removeAttribute("class", "hidden");
+      }
 
+    //document.querySelector("span#name").innerText = nameInput;
+    //document.querySelector("animal").innerText = animal;
+    //document.querySelector("food").innerText = food;
 
-    document.querySelector("nameInput").innerText = nameInput;
-    document.querySelector("animal").innerText = animal;
-    document.querySelector("food").innerText = food;
-
-    result.removeAttribute("class");
+    result.removeAttribute("class", "hidden");
     e.preventDefault();
   });
+}); 
 
+  let form = document.querySelector("form");
   form.addEventListener("submit", function() {
     resetBtn.removeAttribute("class");
   });
 
-  resetBtn.addEventListener("click", function() {
+  resetBtn.addEventListener("click", function(e) {
     result.setAttribute("class", "hidden");
     document.getElementById("nameInput").value = null;
     document.getElementById("animal").value = null;
     document.getElementById("food").value = null;
+    document.getElementById("what").value = null;
+    document.getElementById("hobby").value = null;
   });
 
-});
