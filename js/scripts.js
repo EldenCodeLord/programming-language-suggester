594ad618b33c8b91dff117607d09da6020be4a1d
Manuel Raygoza <manny.raygoza@gmail.com>
2022-08-31 17:30:14 -0700

    WIP: Trying to fix broken js code.

--- js/scripts.js
+++ js/scripts.js
@@ -8,21 +8,28 @@ function hideResult() {
   document.getElementById("css").setAttribute("class", "hidden");
   document.getElementById("js").setAttribute("class", "hidden");
 
-resetBtn.addEventListener("click", function() {
-  result.setAttribute("class", "hidden");
-  document.getElementById("nameInput").value = null;
-  document.getElementById("animal").value = null;
-  document.getElementById("food").value = null;
-  document.getElementById("what").value = null;
-  document.getElementById("hobby").value = null;
 
+function handleFormCheck(event) {
+  event.preventDefault();
+  const selections = document.querySelectorAll()
+}
 
 window.addEventListener("load", function() {
+  document.querySelector("form")
   let form = document.querySelector("form");
   let resetBtn = document.querySelector("button#reset");
   let result = document.querySelector("div#result");
 
-  form.addEventListener("submit", function() { 
+  resetBtn.addEventListener("click", function() {
+    result.setAttribute("class", "hidden");
+    document.getElementById("nameInput").value = null;
+    document.getElementById("cat").value = null;
+    document.getElementById("food").value = null;
+    document.getElementById("what").value = null;
+    document.getElementById("hobby").value = null;
+
+
+  form.addEventListener("submit", function(event) { 
     const nameInput = document.getElementById("nameInput").value;
     const fish = document.getElementById("js").value;
     const cat = document.getElementById("css").value;
@@ -36,12 +43,8 @@ window.addEventListener("load", function() {
       }
 
     result.removeAttribute("class", "hidden");
-    e.preventDefault();
+    event.preventDefault();
   }); 
-
-  form.addEventListener("submit", function() {
-    resetBtn.removeAttribute("class");
-  });
 });
 });
 }
